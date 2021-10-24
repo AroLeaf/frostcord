b96f7d2ab54007faf0e60c3222c7478d8eef6997
AroLeaf <starleafofa@gmail.com>
2021-10-24 14:48:00 +0200

    moved embed author to meta

--- src/EmbedFactory.js
+++ src/EmbedFactory.js
@@ -11,10 +11,10 @@ const regex = {
   thumbnail: XRegExp('tmb:([^\\s]*)'),
   color: XRegExp('clr:0x([0-9a-f]+)'),
   timestamp: XRegExp('tm:(\\d+)'),
+  author: XRegExp('aut:(!\\[(\\[(?<texta>.*)\\]\\((?<urla>.*)\\)|(?<textb>.*))\\]\\((?<imagea>.*)\\)|^(\\[(?<textc>.*)\\]\\((?<urlb>.*)\\)|(?<textd>.*)))', 'n'),
 
   parts: XRegExp('^[^\\S\\r\\n]*#', 'm'),
   url: XRegExp('\\[(?<texta>.*)\\]\\((?<urla>.*)\\)|(?<textb>.*)'),
-  author: XRegExp('^!\\[(\\[(?<texta>.*)\\]\\((?<urla>.*)\\)|(?<textb>.*))\\]\\((?<imagea>.*)\\)|^(\\[(?<textc>.*)\\]\\((?<urlb>.*)\\)|(?<textd>.*))', 'n'),
   title: XRegExp(`^ (\\[(?<texta>.*)\\]\\((?<urla>.*)\\)|(?<textb>.*))\\n[^\\S\\r\\n]*(?<bodya>[^]*)`, 'n'),
   field: XRegExp('(?<=.)(.*)\\n[^\\S\\r\\n]*([^]*)'),
 }
@@ -52,16 +52,16 @@ export default class EmbedFactory {
     const image = XRegExp.exec(meta, regex.image)?.[1];
     const thumbnail = XRegExp.exec(meta, regex.thumbnail)?.[1];
     const color = XRegExp.exec(meta, regex.color)?.[1];
-    const timestamp = XRegExp.exec(meta, regex.timestamp)?.[1];
-  
-    const parts = body.split(regex.parts);
-    
-    const authorMatch = XRegExp.exec(parts.shift(), regex.author);
+    const timestamp = XRegExp.exec(meta, regex.timestamp)?.[1];4
+
+    const authorMatch = XRegExp.exec(meta, regex.author);
     const author = {
       name: authorMatch?.groups.texta || authorMatch?.groups.textb || authorMatch?.groups.textc || authorMatch?.groups.textd,
       url: authorMatch?.groups.urla || authorMatch?.groups.urlb,
       icon_url: authorMatch?.groups.imagea,
     }
+  
+    const parts = body.split(regex.parts);
   
     const titleMatch = XRegExp.exec(parts.shift(), regex.title);
     const title = titleMatch.groups.texta || titleMatch.groups.textb;
